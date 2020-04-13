70cdff85fed518b6ddcf00755e617758e1eec03f
Seth Wheeler <wheeler.seth@gmail.com>
2020-04-13 13:11:42 -0600

    items can be sorted ascending & descending off likes

--- client/src/Pages/home.page.jsx
+++ client/src/Pages/home.page.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const HomePage = () => {
   
   const [ items, setItems ] = useState([])
-
+  const [ sortAsc, setSort] = useState(false)
   useEffect(()=>{
     axios.get('/api/items')
     .then(res => setItems(res.data))
@@ -13,6 +13,11 @@ const HomePage = () => {
   }, [])
 
   const renderItems = () =>{
+    if(sortAsc){
+    items.sort((a,b) => a.likes - b.likes);
+  }else{
+    items.sort((a,b) => b.likes - a.likes);
+  }
     return items.map( item => (
       <div key={`item-${item.id}`}>
       <h3> {item.name}</h3>
@@ -23,6 +28,14 @@ const HomePage = () => {
     ))
   }
 
+  const toggleSort = () => setSort(!sortAsc)
+
+  const sortButton = ()=> {
+    if(!sortAsc){
+    return <button onClick={toggleSort}> Sort Asc </button>
+   }else{ 
+    return <button onClick={toggleSort}> Sort Desc </button>
+   }}
 
   return(
    <div>
@@ -30,6 +43,9 @@ const HomePage = () => {
      <Link to={{pathname:'/addItem'}}> 
         <button> Add Item </button>
      </Link>
+     <br />
+     <br />
+     {sortButton()}
      <br />
      {renderItems()}
    </div>
